71de637e192f7262cdcec227ca7f35eea8d9231b
Python Hub <pythonhub.py@gmail.com>
2020-09-02 00:30:31 +0530

    Audio sendig also added

--- main.py
+++ main.py
@@ -24,13 +24,22 @@ mail['Subject'] = 'This is a test message'
 mail.attach(MIMEText("This a test mail sent with python by pythonhub.", 'plain'))
 
 image = open('image.jpg', 'rb')
+# audio = open('sample.mp3', 'rb') // Send Audio
 
 attachment = MIMEBase('application', 'octet-stream')
 
+
 attachment.set_payload(image.read())
+# attachment.set_payload(audio.read()) // Send Audio
 
 encoders.encode_base64(attachment)
-attachment.add_header('Content-Description', f'attachment, filename=image.jpg')
+
+attachment.add_header('Content-Disposition',
+                      'attachment; filename=image.jpg')
+
+# attachment.add_header('Content-Disposition',
+#   'attachment; filename=sample.mp3') // Send Audio
+
 mail.attach(attachment)
 
 message = mail.as_string()
